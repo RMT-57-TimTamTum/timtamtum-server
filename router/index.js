04dfe32518cc9b4043815a6bceaef3ab2b74bcc7
Fachri Hawari <fachri.hawari@gmail.com>
2025-02-04 13:40:01 +0700

    feat: Register api done

--- router/index.js
+++ router/index.js
@@ -1,8 +1,11 @@
 const indexRouter = require('express').Router()
+const authRouter = require('./auth')
 
 indexRouter.get('/', (req, res) => {
   res.send("Hello")
 })
+
+indexRouter.use(authRouter)
 
 module.exports = indexRouter
 
